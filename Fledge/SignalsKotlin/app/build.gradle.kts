4b76b57030a65e528061226ed1036466c6060f71
Caleb Clark <calebclark@google.com>
2023-12-26 19:22:19 +0000

    Add Kotlin signals sample app
    Change-Id: I20fa6e357120344bca23018f31b95be66ee88ccf

---
+++ Fledge/SignalsKotlin/app/build.gradle.kts
@@ -0,0 +1,64 @@
+plugins {
+  id("com.android.application")
+  id("org.jetbrains.kotlin.android")
+}
+
+android {
+  namespace = "com.example.adservices.samples.signals.sampleapp"
+  compileSdkPreview = "UpsideDownCakePrivacySandbox"
+
+  defaultConfig {
+    applicationId = "com.example.adservices.samples.signals.sampleapp"
+    minSdkPreview = "UpsideDownCakePrivacySandbox"
+    targetSdkPreview = "UpsideDownCakePrivacySandbox"
+    versionCode = 1
+    versionName = "1.0"
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    vectorDrawables {
+      useSupportLibrary = true
+    }
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+  kotlinOptions {
+    jvmTarget = "1.8"
+  }
+  buildFeatures {
+    compose = true
+    viewBinding = true
+  }
+  composeOptions {
+    kotlinCompilerExtensionVersion = "1.4.3"
+  }
+  packaging {
+    resources {
+      excludes += "/META-INF/{AL2.0,LGPL2.1}"
+    }
+  }
+}
+
+dependencies {
+
+  implementation("androidx.core:core-ktx:1.9.0")
+  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+  implementation("androidx.activity:activity-compose:1.7.0")
+  implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+  implementation("androidx.compose.ui:ui")
+  implementation("androidx.compose.ui:ui-graphics")
+  implementation("androidx.compose.ui:ui-tooling-preview")
+  implementation("androidx.compose.material3:material3")
+  implementation("androidx.appcompat:appcompat:1.6.1")
+  implementation("com.google.android.material:material:1.11.0")
+  implementation("com.google.guava:guava:29.0-android")
+  implementation("com.google.code.gson:gson:2.10")
+}
