5798233d807035dd564e34bef340b207a8821d13
Anagha Sasikumar <anaghask@google.com>
2024-07-29 15:02:58 +0000

    Load interstitial ad.

--- PrivacySandboxKotlin/existing-sdk/src/main/java/com/existing/sdk/ExistingSdk.kt
+++ PrivacySandboxKotlin/existing-sdk/src/main/java/com/existing/sdk/ExistingSdk.kt
@@ -3,8 +3,11 @@ package com.existing.sdk
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.privacysandbox.activity.client.createSdkActivityLauncher
 import androidx.privacysandbox.sdkruntime.client.SdkSandboxManagerCompat
 import androidx.privacysandbox.sdkruntime.core.LoadSdkCompatException
+import com.example.api.SdkInterstitialRequest
 import com.example.api.SdkService
 import com.example.api.SdkServiceFactory
 
@@ -20,6 +23,13 @@ class ExistingSdk(private val context: Context) {
     suspend fun createFile(size: Int): String? {
         if (!isSdkLoaded()) return null
         return loadSdkIfNeeded(context)?.createFile(size)
+    }
+
+    suspend fun showInterstitialAd(baseActivity: AppCompatActivity): Boolean {
+        if (!isSdkLoaded()) return false
+        val launcher = baseActivity.createSdkActivityLauncher { true }
+        loadSdkIfNeeded(context)?.getInterstitial(launcher)
+        return true
     }
 
     /** Keeps a reference to a sandboxed SDK and makes sure it's only loaded once. */
