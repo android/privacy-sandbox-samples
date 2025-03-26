7682389ef32aa9ccf2456d1ca7cba2b273e70554
Maria Zoe Lopez-Latorre <68270812+notmariazoe@users.noreply.github.com>
2025-03-26 11:27:08 +0000

    docs: fixed some definitions on the KDoc

--- PrivacySandboxKotlin/runtime-enabled-sdk/src/main/java/com/runtimeenabled/implementation/SdkSandboxedUiAdapterImpl.kt
+++ PrivacySandboxKotlin/runtime-enabled-sdk/src/main/java/com/runtimeenabled/implementation/SdkSandboxedUiAdapterImpl.kt
@@ -62,13 +62,13 @@ class SdkSandboxedUiAdapterImpl(
      * The session will handle notifications from and to the client.
      * We consider the client the owner of the SandboxedSdkView.
      *
-     * @param context The application context.
+     @param context The client's context.
      * @param sessionConstants Constants related to the session, such as the presentation id.
-     * @param initialWidth The initial width of the session's view.
-     * @param initialHeight The initial height of the session's view.
+     * @param initialWidth The initial width of the adapter's view.
+     * @param initialHeight The initial height of the adapter's view.
      * @param isZOrderOnTop Whether the session's view should be drawn on top of other views.
-     * @param clientExecutor The executor to run callbacks on the client's thread.
-     * @param client The client to notify about the session events.
+     * @param clientExecutor The executor to use for client callbacks.
+     * @param client A UI adapter representing the client of this single session.
      */
     override fun openSession(
         context: Context,
